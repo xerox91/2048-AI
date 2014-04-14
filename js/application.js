61e7a85c90803a217ae063d3bc747ee3801dd9db
Sergio Salomón <sergio.salomon at alumnos.unican.es>
2014-04-14 20:59:44 +0200

    First commit
    Some cleaning and refactoring over the original version

--- js/application.js
+++ js/application.js
@@ -1,7 +1,15 @@
-animationDelay = 100;
-minSearchTime = 100;
+
+// Configurable values of the game
+GAME_SIZE = 4;
+START_TILES = 2;
+USUAL_TILE_VAL = 2;
+BONUS_TILE_VAL = 4;
+GOAL_VAL = 2048;
+ANIMATION_DELAY = 600;
+AI_SEARCH_TIME = 300;  // for AI algorithms
+
 
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  var manager = new GameManager(4, KeyboardInputManager, HTMLActuator);
+    var manager = new GameManager(GAME_SIZE, KeyboardInputManager, HTMLActuator);
 });
